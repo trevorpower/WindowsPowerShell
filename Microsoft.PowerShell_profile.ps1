dd5472023c3d5a45c9b710a555f3c29540ef5912
Trevor Power <trevor_power@yahoo.com>
2015-07-08 22:20:53 +0100

    dsiplay '~' for home directory

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,15 @@ function prompt()
   $principal = [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
   $isAdmin = $principal.IsInRole([Security.Principal.windowsBuiltInRole] "Administrator")
 
-  $dir = (Get-Location | Split-Path -Leaf)
+  $location = Get-Location
+
+  if ($location.Path -eq $env:HOMEDRIVE + $env:HOMEPATH) {
+    $dir = '~'
+  }
+  else {
+    $dir = Split-Path $location -Leaf
+  }
+
   $char = @{$true='#';$false='>'}[$isAdmin]
 
   return "$dir$char ";
