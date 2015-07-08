08f9523dd27849f76a62e22b561dbe1fb0fc8069
Trevor Power <trevor_power@yahoo.com>
2015-07-08 21:56:28 +0100

    fixed up spaces in source

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,10 @@
 function prompt()
 {
-	$principal = [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
-	$isAdmin = $principal.IsInRole([Security.Principal.windowsBuiltInRole] "Administrator")
+  $principal = [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+  $isAdmin = $principal.IsInRole([Security.Principal.windowsBuiltInRole] "Administrator")
 
-	$dir = (Get-Location | Split-Path -Leaf)
-	$char = @{$true='#';$false='>'}[$isAdmin]
+  $dir = (Get-Location | Split-Path -Leaf)
+  $char = @{$true='#';$false='>'}[$isAdmin]
 
-	return "$dir$char ";
+  return "$dir$char ";
 }
