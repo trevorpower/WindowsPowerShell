8b47bcf9968f065d8a23c05568745dba705b4881
Trevor Power <trevor_power@yahoo.com>
2015-07-08 21:20:37 +0100

    added simple profile to change prompt

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+function prompt()
+{
+	$principal = [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+	$isAdmin = $principal.IsInRole([Security.Principal.windowsBuiltInRole] "Administrator")
+
+	$dir = (Get-Location | Split-Path -Leaf)
+	$char = @{$true='#';$false='>'}[$isAdmin]
+
+	return "$dir$char ";
+}
