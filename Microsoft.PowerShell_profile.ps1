54618694a42ab7b9af32d45713df0dccbb8eb59b
Trevor Power <trevor_power@yahoo.com>
2015-07-09 12:08:05 +0100

    add visual studio path if available

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,12 @@
+&{
+  $vs = "${env:ProgramFiles(x86)}\Microsoft Visual Studio 12.0\Common7\IDE"
+  $found = test-path($vs)
+  if ($found) {
+	write-host "Visual Studio Available"
+	$env:PATH += ";$vs"
+  }
+}
+
 function prompt()
 {
   $principal = [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
