61f76f0e37af2899b1350576be7430dfb1bb3745
Trevor Power <trevor_power@yahoo.com>
2015-07-10 21:15:01 +0100

    move prompt into inner scope

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,26 +2,27 @@
   $vs = "${env:ProgramFiles(x86)}\Microsoft Visual Studio 12.0\Common7\IDE"
   $found = test-path($vs)
   if ($found) {
-	write-host "Visual Studio Available"
-	$env:PATH += ";$vs"
+    write-host "Visual Studio Available"
+    $env:PATH += ";$vs"
+  }
+
+  function global:prompt()
+  {
+    $principal = [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+    $isAdmin = $principal.IsInRole([Security.Principal.windowsBuiltInRole] "Administrator")
+
+    $location = Get-Location
+
+    if ($location.Path -eq $env:HOMEDRIVE + $env:HOMEPATH) {
+      $dir = '~'
+    }
+    else {
+      $dir = Split-Path $location -Leaf
+    }
+
+    $char = @{$true='#';$false='>'}[$isAdmin]
+
+    return "$dir$char ";
   }
 }
 
-function prompt()
-{
-  $principal = [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
-  $isAdmin = $principal.IsInRole([Security.Principal.windowsBuiltInRole] "Administrator")
-
-  $location = Get-Location
-
-  if ($location.Path -eq $env:HOMEDRIVE + $env:HOMEPATH) {
-    $dir = '~'
-  }
-  else {
-    $dir = Split-Path $location -Leaf
-  }
-
-  $char = @{$true='#';$false='>'}[$isAdmin]
-
-  return "$dir$char ";
-}
