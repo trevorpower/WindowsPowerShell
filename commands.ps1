c53eff57a87f7af73ca40e1a17335d11a28d2223
Trevor Power <trevor.power@kneat.com>
2015-12-11 11:36:17 +0000

    added rdp command

--- commands.ps1
+++ commands.ps1
@@ -1,22 +1,35 @@
 function global:Pop-Directory(){
-  Set-Location ..
+   Set-Location ..
 }
 
 function global:Reset-Directory(){
-  Set-Location ~
+   Set-Location ~
 }
 
 function global:Find-Aliases($definition){
-  Get-Alias -Definition *$definition*
+   Get-Alias -Definition *$definition*
 }
 
 function global:Stop-ProcessByName($name){
-  Stop-Process -Name $name
+   Stop-Process -Name $name
 }
 
+function global:Stop-ProcessByName($name){
+   Stop-Process -Name $name
+}
+
+function global:Connect-Machine($name){
+   if (-Not $name) {
+      Get-ChildItem ~/.rdp | %{ $_.BaseName }
+   }
+   else {
+      mstsc (Get-Item "~/.rdp/$name.rdp").Fullname
+   }
+}
 
 Set-Alias ~ Reset-Directory
 Set-Alias .. Pop-Directory
 Set-Alias al Find-Aliases
 Set-Alias cull Stop-ProcessByName
 Set-Alias sync Sync-PSProfile
+Set-Alias rdp Connect-Machine
